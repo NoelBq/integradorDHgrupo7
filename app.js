10b92f92bd5dcd006cd42c617fc0786c5fe3404c
Noelia <noeliabaquerostaci@gmail.com>
2021-08-19 15:36:41 -0300

    folder structure created

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+app.use(express.static(path.join(__dirname, "./public")));
+
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/home.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/register.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"));
+});
+
+app.get("/shop", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/shop.html"));
+  });
+
+  app.get("/product", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/product.html"));
+  });
+  
+
+app.get("/cart", (req, res) => {
+    res.sendFile(path.join(__dirname, "./view/cart.html"));
+  });
+  
+
+const port = process.env.PORT || '3000';
+
+app.listen(port,()=>{
+
+console.log(`Server is runnig in the Port : ${port}`);
+})
