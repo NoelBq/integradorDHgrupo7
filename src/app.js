71640b3a12ddb4c2044b993dde5e2572b1376406
DESKTOP-KQT5V53\Noelia <noeliabaquerostaci@gmail.com>
2021-09-11 18:05:35 -0300

    changes app js

--- src/app.js
+++ src/app.js
@@ -2,14 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mainRoutes = require('./routes/mainRoutes');
-const PORT = process.env.PORT || '3000';
 app.use(express.static(path.join(__dirname, "./public")));
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "./views"));
-
 app.use('/', mainRoutes);
 
-app.listen(PORT,()=>{
-
-console.log(`Server is runnig in the Port : ${PORT}`);
+app.use((req,res,next) => {
+    res.status(404).render('error')
 })
+module.exports = app;
