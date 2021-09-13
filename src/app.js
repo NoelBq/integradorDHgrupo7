32b969f3d88f1d8f34cd3efb715f63a31518338d
DESKTOP-KQT5V53\Noelia <noeliabaquerostaci@gmail.com>
2021-09-13 00:43:02 -0300

    fix: routes error

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "./views"));
 app.use('/', mainRoutes);
 
+
 app.use((req,res,next) => {
     res.status(404).render('error')
 })
