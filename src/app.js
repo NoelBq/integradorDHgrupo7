dabc932804a53411e8720e269894754f7b0f9d51
DESKTOP-KQT5V53\Noelia <noeliabaquerostaci@gmail.com>
2021-09-14 22:39:50 -0300

    fix: broken routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+
 const mainRoutes = require('./routes/mainRoutes');
+const productRoutes = require('./routes/productRoutes');
 app.use(express.static(path.join(__dirname, "./public")));
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "./views"));
 app.use('/', mainRoutes);
-
+app.use('/product', productRoutes);
 
 app.use((req,res,next) => {
     res.status(404).render('error')
