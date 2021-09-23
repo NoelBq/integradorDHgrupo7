75787bac36ffe4dad02f628357dacb2bd28e7442
DESKTOP-KQT5V53\Noelia <noeliabaquerostaci@gmail.com>
2021-09-23 20:43:23 -0300

    adds multer to the projects

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,6 +3,22 @@ const router = express.Router();
 const productController = require('../controllers/productsController');
 const productsDB  = require('../../db/productsDatabase.json');
 
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../public/img"));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix =
+      Date.now() + "-" + Math.round(Math.random() * 1e9) + file.originalname;
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage: storage });
+
 router.get('/:id', productController.product);
 router.delete('/:id/delete', productController.deleteproduct);
 
