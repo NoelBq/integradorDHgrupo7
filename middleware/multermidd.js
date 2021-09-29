d6cd274670bb9b4ffe7a62e0d3d97584676281b5
Juancho <Juanmmarengo@gmail.com>
2021-09-29 15:26:19 -0300

    mod: ajuste en multermidd y ajuste en ProductsRouter

--- middleware/multermidd.js
+++ middleware/multermidd.js
@@ -3,14 +3,13 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, '../src/public/images'))
+      cb(null, path.join(__dirname, "../public/images"));
     },
     filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)+file.originalname;
-      cb(null, file.fieldname + '-' + uniqueSuffix)
+      cb(null, Date.now() + "-" + file.originalname);
     }
-  })
+  });
   
-  const upload = multer({ storage: storage })
+  const upload = multer({ storage: storage });
 
   module.exports = upload;
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -2,25 +2,13 @@ const express = require("express");
 const router = express.Router();
 const productController = require('../controllers/productsController');
 const productsDB  = require('../../db/productsDatabase.json');
-
-const multer = require("multer");
-const path = require("path");
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "../public/images"));
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  }
-});
-
-const upload = multer({ storage: storage });
+const upload = require('../../middleware/multermidd')
 
 router.get('/:id', productController.product);
 router.delete('/:id/delete', productController.deleteproduct);
 
 router.get('/edit/:id', productController.productEditView);
 router.put('/edit/:id',upload.single("img"), productController.productEdit);
+router.post('/',upload.single('addimages'), productController.insert)
 
 module.exports = router;
