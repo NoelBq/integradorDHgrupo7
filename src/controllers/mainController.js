1e24dfa711eeab82d3b84663d73de3e3ca04c8ba
Juancho <Juanmmarengo@gmail.com>
2021-10-17 18:55:36 -0300

    add: terms services

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,45 +1,65 @@
-const testimonialsDB  = require('../../db/testimonialDatabase.json');
-const productsDB  = require('../../db/productsDatabase.json');
-const utils = require('../utils/utils');
-const fs = require('fs');
-const path = require('path');
-
+const testimonialsDB = require("../../db/testimonialDatabase.json");
+const productsDB = require("../../db/productsDatabase.json");
+const utils = require("../utils/utils");
+const fs = require("fs");
+const path = require("path");
 
 const mainController = {
     home: (req, res) => {
-        res.render('home', {testimonials: testimonialsDB});
+        res.render("home", { testimonials: testimonialsDB });
     },
-    forms: (req, res) => {
-        res.render('forms');
+    login: (req, res) => {
+        res.render("formlogin");
+    },
+    register: (req, res) => {
+        res.render("formregister");
+    },
+    terms: (req, res) => {
+        res.render("terms");
     },
     shop: (req, res) => {
-        res.render('shop', {testimonials: testimonialsDB, products: productsDB});
+        res.render("shop", { testimonials: testimonialsDB, products: productsDB });
     },
-    shopDonas: (req,res) =>{
+    shopDonas: (req, res) => {
         let localProductsDB = utils.parseJS(productsDB);
-      res.render('shopDonas', {testimonials: testimonialsDB, products: localProductsDB})
+        res.render("shopDonas", {
+            testimonials: testimonialsDB,
+            products: localProductsDB,
+        });
     },
-    shopCookies: (req,res) =>{
+    shopCookies: (req, res) => {
         let localProductsDB = utils.parseJS(productsDB);
-        res.render('shopCookies', {testimonials: testimonialsDB, products: localProductsDB})
-      },
-    shopHelados: (req,res) =>{
+        res.render("shopCookies", {
+            testimonials: testimonialsDB,
+            products: localProductsDB,
+        });
+    },
+    shopHelados: (req, res) => {
         let localProductsDB = utils.parseJS(productsDB);
-        res.render('shopHelados', {testimonials: testimonialsDB, products: localProductsDB})
-      },
+        res.render("shopHelados", {
+            testimonials: testimonialsDB,
+            products: localProductsDB,
+        });
+    },
     underConstruction: (req, res) => {
-        res.render('underconstruction');
+        res.render("underconstruction");
     },
     adminpanel: (req, res) => {
-        let localProductsDB = JSON.parse(fs.readFileSync(path.join(__dirname,'../../db/productsDatabase.json'), {encoding:'utf8', flag:'r'})); 
-        let categories = [...new Set(localProductsDB.map(p => p.category))];
-        let products = localProductsDB.filter(p => p.category == req.query.categorySelection);
-        res.render('adminpanel', {products: products, categories: categories});
+        let localProductsDB = JSON.parse(
+            fs.readFileSync(path.join(__dirname, "../../db/productsDatabase.json"), {
+                encoding: "utf8",
+                flag: "r",
+            })
+        );
+        let categories = [...new Set(localProductsDB.map((p) => p.category))];
+        let products = localProductsDB.filter(
+            (p) => p.category == req.query.categorySelection
+        );
+        res.render("adminpanel", { products: products, categories: categories });
     },
     formsadmin: (req, res) => {
-        res.render('formsadmin');
-    }
-
-}
+        res.render("formsadmin");
+    },
+};
 
 module.exports = mainController;
