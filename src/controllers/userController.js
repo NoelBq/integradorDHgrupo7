356c055209e9d75a9d5f17c13d8d166c0360a7de
NoelBq <noeliabaquerostaci@gmail.com>
2021-10-25 16:08:43 -0300

    adds functionality to login and register

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,24 +1,69 @@
 
 const User = require('../../models/User'); 
 const bcryptjs = require('bcryptjs');
-const {validationResult} = require('express-validator')
+const {validationResult} = require('express-validator');
+const e = require('express');
 
 
 const userController = {
+
+    userProfile: (req, res) => {
+        res.render('userprofile')
+    },
     processRegister: (req, res) => {
         const errors = validationResult(req);
+        let hashedPassword = bcryptjs.hashSync(req.body.password, 10);
+        let userInDB = User.findByField('email', req.body.email);
         if (!errors.isEmpty()) {
             const validations = errors.array();
-            res.render('formregister', { validations: validations })
+            let filteredValidations = validations.filter(err => err.msg != 'Invalid value')
+            const oldData = req.body;
+            res.render('formregister', { validations: filteredValidations })
             console.log(errors)
-        } else {
-            let userToCreate = {
-                ...req.body,
-                rol: "basic",
-                password: bcryptjs.hashSync(req.body.password, 10)
+        } 
+
+        if(userInDB) {
+            res.render('formregister', {
+                errors: 
+                {
+                    email: {
+                        msg: "Usuario ya registrado"
+                    }
+                },
+                oldData: req.body // to keep data previously
+            })
+        } 
+
+        let userToCreate = {
+            ...req.body,
+            rol: "basic",
+            password: hashedPassword
+        }
+        User.createUser(userToCreate);
+        res.status(200).redirect('login')
+    
+    },
+
+    loginProcess: (req, res) => {
+        let userToLoggin = User.findByField('email', req.body.email)
+        if(userToLoggin) {
+            let passwordOK = bcryptjs.compareSync(req.body.password, userToLoggin.password);
+            if(passwordOK) {
+                if(userToLoggin.rol == 'admin') {
+                    res.redirect('/adminpanel')
+                } else {
+                    res.render('userprofile',{ user: userToLoggin})
+                }
             }
-            User.createUser(userToCreate);
-            res.status(200).redirect('/')
+        } else {
+            return res.render('formlogin', {
+                errors: 
+                    {
+                        email: {
+                            msg: "Credenciales Invalidas"
+                        }
+                    },
+            })
         }
     }
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const { body } = require('express-validator');
 const validations = require('../middleware/validation')
+const upload = require('../middleware/userMulterMidd');
 
 // This router is for /user prefix "/" is /user
-router.post('/', validations , userController.processRegister);
+router.post('/', validations, userController.processRegister);
+router.post('/login' ,userController.loginProcess);
+router.get('/profile', userController.userProfile);
 
 module.exports = router;
