6eed660f96976390982672b78faaadf6086beb87
Juancho <Juanmmarengo@gmail.com>
2021-10-26 19:21:08 -0300

    fix loginProcess else credentials

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,74 +1,72 @@
-
-const User = require('../../models/User'); 
-const bcryptjs = require('bcryptjs');
-const {validationResult} = require('express-validator');
-const e = require('express');
-
+const User = require("../../models/User");
+const bcryptjs = require("bcryptjs");
+const { validationResult } = require("express-validator");
+const e = require("express");
 
 const userController = {
-
     userProfile: (req, res) => {
-        res.render('userprofile')
+        res.render("userprofile");
     },
     processRegister: (req, res) => {
         const errors = validationResult(req);
         let hashedPassword = bcryptjs.hashSync(req.body.password, 10);
-        let userInDB = User.findByField('email', req.body.email);
+        let userInDB = User.findByField("email", req.body.email);
         if (!errors.isEmpty()) {
             const validations = errors.array();
-            let filteredValidations = validations.filter(err => err.msg != 'Invalid value')
+            let filteredValidations = validations.filter(
+                (err) => err.msg != "Invalid value"
+            );
             const oldData = req.body;
-            res.render('formregister', { validations: filteredValidations })
-            console.log(errors)
-        } 
+            res.render("formregister", { validations: filteredValidations });
+            console.log(errors);
+        }
 
-        if(userInDB) {
-            res.status(409).render('formregister', {
-                errors: 
-                {
+        if (userInDB) {
+            res.status(409).render("formregister", {
+                errors: {
                     email: {
-                        msg: "Usuario ya registrado"
-                    }
+                        msg: "Usuario ya registrado",
+                    },
                 },
-                oldData: req.body // to keep data previously added
-            })
+                oldData: req.body, // to keep data previously added
+            });
         } else {
             let userToCreate = {
                 ...req.body,
                 rol: "basic",
                 password: hashedPassword,
-                image: req.file.filename
-            }
+                image: req.file.filename,
+            };
             User.createUser(userToCreate);
-            res.status(200).redirect('login?registered=1')
+            res.status(200).redirect("login?registered=1");
         }
-    
     },
 
     loginProcess: (req, res) => {
-        let userToLogin = User.findByField('email', req.body.email)
-        if(userToLoggin) {
-            req.session.userLoged = userToLogin; 
-            let passwordOK = bcryptjs.compareSync(req.body.password, userToLogin.password);
-            if(passwordOK) {
-                if(userToLoggin.rol == 'admin') {
-                    res.redirect('/adminpanel')
+        let userToLogin = User.findByField("email", req.body.email);
+        if (userToLogin) {
+            req.session.userLoged = userToLogin;
+            let passwordOK = bcryptjs.compareSync(
+                req.body.password,
+                userToLogin.password
+            );
+            if (passwordOK) {
+                if (userToLogin.rol == "admin") {
+                    res.redirect("/adminpanel");
                 } else {
-                    res.render('userprofile',{ user: userToLoggin})
+                    res.render("userprofile", { user: userToLogin });
                 }
-            }
-        } else {
-            return res.render('formlogin', {
-                errors: 
-                    {
+            } else {
+                return res.render("formlogin", {
+                    errors: {
                         email: {
-                            msg: "Credenciales Invalidas"
-                        }
+                            msg: "Credenciales Invalidas",
+                        },
                     },
-            })
+                });
+            }
         }
-    }
-}
+    },
+};
 
 module.exports = userController;
-
