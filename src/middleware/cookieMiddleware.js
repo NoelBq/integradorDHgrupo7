fb0af676a9f9aa8af7e51fb40be0ddd103eb9ad6
NoelBq <noeliabaquerostaci@gmail.com>
2021-11-01 01:47:32 -0300

    adds products to database

--- src/middleware/cookieMiddleware.js
+++ src/middleware/cookieMiddleware.js
@@ -4,7 +4,6 @@ const User = require('../../models/User');
 function rememberUser(req, res, next)  {
     let emailCookie = req.cookies.userEmail;
     let userFromCookie = User.findByField('email', emailCookie);
-    console.log(userFromCookie);
     if(userFromCookie) {
         req.session.userLogged = userFromCookie;
     }
