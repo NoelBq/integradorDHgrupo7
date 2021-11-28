282d2c8e9e8251b8b2a42f6bac6badb31e6a799e
NoelBq <noeliabaquerostaci@gmail.com>
2021-11-27 22:43:00 -0300

    fixed products in admin panel

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,72 +1,88 @@
 const testimonialsDB = require("../../db/testimonialDatabase.json");
-const product = require("../models/Product");
+const products = require("../models/Product");
+const categories = require('../models/Categories');
 const utils = require("../utils/utils");
 const fs = require("fs");
 const path = require("path");
+const { ResultWithContext } = require("express-validator/src/chain");
 
 const mainController = {
-    home: (req, res) => {
-        res.render("home", { testimonials: testimonialsDB , user: req.session.userLogged });
-    },
-    login: (req, res) => {
-        res.render("formlogin", {registered: req.query.registered, user: req.session.userLogged});
-    },
-    register: (req, res) => {
-        res.render("formregister", {user: req.session.userLogged });
-    },
-    terms: (req, res) => {
-        res.render("terms");
-    },
-    shop: async (req, res) => {
-        const products = await product.getAllProducts();
-        res.render("shop", { testimonials: testimonialsDB, products: products, user: req.session.userLogged });
-    },
-    shopDonas: async (req, res) => {
-        const products = await product.getAllProducts();
-        res.render("shopDonas", {
-            testimonials: testimonialsDB,
-            products: products,
-            user: req.session.userLogged
-        });
-    },
-    shopCookies: async (req, res) => {
-        const products = await product.getAllProducts();
-        res.render("shopCookies", {
-            testimonials: testimonialsDB,
-            products: products,
-            user: req.session.userLogged
-        });
-    },
-    shopHelados: async (req, res) => {
-        const products = await product.getAllProducts();
-        res.render("shopHelados", {
-            testimonials: testimonialsDB,
-            products: products,
-            user: req.session.userLogged
-        });
-    },
-    underConstruction: (req, res) => {
-        res.render("underconstruction"), {user: req.session.userLogged};
-    },
-    adminpanel: (req, res) => {
-        let localProductsDB = JSON.parse(
-            fs.readFileSync(path.join(__dirname, "../../db/productsDatabase.json"), {
-                encoding: "utf8",
-                flag: "r",
-            })
-        );
-        let categories = [...new Set(localProductsDB.map((p) => p.category))];
-        let products = localProductsDB.filter(
-            (p) => p.category == req.query.categorySelection
-        );
-        res.render("adminpanel", { products: products, categories: categories, user: req.session.userLogged});
-    },
-    formsadmin: (req, res) => {
-        res.render("formsadmin", {user: req.session.userLogged});
-    },
-    checkout: (req, res) => {
-        res.render("checkout", {user: req.session.userLogged});
-    }
+	home: (req, res) => {
+		res.render("home", { testimonials: testimonialsDB , user: req.session.userLogged });
+	},
+	login: (req, res) => {
+		res.render("formlogin", {registered: req.query.registered, user: req.session.userLogged});
+	},
+	register: (req, res) => {
+		res.render("formregister", {user: req.session.userLogged });
+	},
+	terms: (req, res) => {
+		res.render("terms");
+	},
+	shop: async (req, res) => {
+		const products = await product.getAllProducts();
+		res.render("shop", { testimonials: testimonialsDB, products: products, user: req.session.userLogged });
+	},
+	shopDonas: async (req, res) => {
+		const products = await product.getAllProducts();
+		res.render("shopDonas", {
+			testimonials: testimonialsDB,
+			products: products,
+			user: req.session.userLogged
+		});
+	},
+	shopCookies: async (req, res) => {
+		let products = await product.getAllProducts();
+		res.render("shopCookies", {
+			testimonials: testimonialsDB,
+			products: products,
+			user: req.session.userLogged
+		});
+	},
+	shopHelados: async (req, res) => {
+		const products = await product.getAllProducts();
+		res.render("shopHelados", {
+			testimonials: testimonialsDB,
+			products: products,
+			user: req.session.userLogged
+		});
+	},
+	underConstruction: (req, res) => {
+		res.render("underconstruction"), {user: req.session.userLogged};
+	},
+	adminpanel: async (req, res) => {
+		let productsData;
+		let categoriesData;
+		let selectedCategory =req.query.categorySelection;
+		console.log(req.query.categorySelection);
+		try {
+			productsData = await products.getAllProducts();
+			categoriesData = await categories.getAllCategories();
+		
+			// if(req.query.categorySelection) {
+			// 	productsData = await products.getProductByCategoryId(req.querySelection)
+			// }
+			// let categorySelection = [...new Set(categories.map((p) => p.category))];
+			// let productsFiltered = products.filter(
+			//     (p) => p.category == req.query.categorySelection
+			// );
+			
+		} catch (error) {
+			console.log(error)
+		}
+		res.render("adminpanel", { 
+			products: productsData, 
+			categories: categoriesData, 
+			user: req.session.userLogged
+		});
+
+	},
+	formsadmin: (req, res) => {
+		res.render("formsadmin", {user: req.session.userLogged});
+	},
+	checkout: (req, res) => {
+		res.render("checkout", {user: req.session.userLogged});
+	}
 };
 
 module.exports = mainController;
--- src/models/Product.js
+++ src/models/Product.js
@@ -6,14 +6,18 @@ const Product = {
                     model: db.sequelize.models.categories, as: "category"
                 } ] } );
         return res;
-       
     },
     getProductByPk: async function(id) {
         const res = await db.products.findByPk(id);
-return res;
+        return res;
+    },
+    getProductByCategoryId: async function(id) {
+        const res = await db.products.findAll(
+            { where: { categoryId: id } }
+        )
+        return res;
     }
 }
 
 
 module.exports = Product;
-console.log(Product.getAllProducts());
