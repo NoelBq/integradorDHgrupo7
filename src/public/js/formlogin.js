9a0b79cf3bd1bf81fb66291bbbcc7e089d4b41a2
Juancho <Juanmmarengo@gmail.com>
2021-12-16 23:38:12 -0300

    validacion del login

---
+++ src/public/js/formlogin.js
@@ -0,0 +1,60 @@
+let formulario = document.querySelector("form.input-register")
+    let inputs = document.querySelectorAll("form input")
+   
+    const expresiones = {
+        email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+        password: /^.{1,40}$/, // 4 a 12 digitos.
+    }
+
+    const campos = {
+        email: false
+    }
+
+    const validarFormulario = (e) => {
+        switch (e.target.name) {
+            case "password":
+                validarCampo(expresiones.password, e.target, "password")
+            break;
+            case "email":
+                validarCampo(expresiones.email, e.target, "email")
+            break;
+        }
+    }
+
+    const validarCampo = (expresiones, input, campo) => {
+        if(expresiones.test(input.value)){
+            document.getElementById(`${campo}`).style.color = "green"
+            document.querySelector(`.grupo__${campo} #input-error-${campo}`).style.display = "none"
+            document.querySelector(`.grupo__${campo} .grupo__inputs i`).style.display = "block"
+            campos[campo] = true;
+        }else{
+            document.getElementById(`${campo}`).style.color = "red"
+            document.querySelector(`.grupo__${campo} #input-error-${campo}`).classList.add("input-error-activo")
+            document.querySelector(`.grupo__${campo} .grupo__inputs i`).style.display = "none"
+            document.querySelector(`.grupo__${campo} #input-error-${campo}`).style.display = "block"
+            campos[campo] = false;
+        }
+    }
+
+    inputs.forEach((input) => {
+        input.addEventListener('keyup', validarFormulario);
+        input.addEventListener('blur', validarFormulario);
+    });
+
+    formulario.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        let terminos = document.getElementById("accept")
+        
+        if(campos.password && campos.email){
+            formulario.submit()
+            formulario.reset();
+            document.querySelector("#formulario__mensaje-exito").style.display = "block"
+            document.querySelector(".formulario__mensaje-error p").style.display = "none"
+            setTimeout(() =>{
+                document.querySelector("#formulario__mensaje-exito").style.display = "none"
+            }, 7000)
+        }else{
+            document.querySelector(".formulario__mensaje-error p").style.display = "block"
+        }
+    })
